923207c752d340c59264a3a5176a8d8df54a0bca
Khumbolawo-fmbch <khumbolawo.mussa@fmbcapitalgroup.com>
2025-11-25 14:36:21 +0200

    Tests: add mra tin verification test

--- tests/api/commons.spec.ts
+++ tests/api/commons.spec.ts
@@ -2,4 +2,29 @@
 import { test, expect, request as playwrightRequest } from "@playwright/test";
 import { generateToken } from "./helpers/generate-token";
 
-let createdAccTypeId: string | number | undefined;
+test.describe("commons API", () => {
+  test("check MRA integration status", async () => {
+    const apiRequestContext = await playwrightRequest.newContext({
+      ignoreHTTPSErrors: true,
+      baseURL: process.env.API_BASE_URL,
+    });
+    // Generate authentication token
+    const token = await generateToken(apiRequestContext);
+    const response = await apiRequestContext.post(
+      "/api/v1/cib-onboarding/commons/verify-tin/",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: {
+          tin: "71018503",
+        },
+      }
+    );
+    expect(response.ok()).toBeTruthy();
+    const body = await response.json();
+    expect(body).toHaveProperty("TaxpayerName");
+    expect(body).toHaveProperty("TPIN");
+    expect(body).toHaveProperty("BusinessRegistrationNumber");
+  });
+});
